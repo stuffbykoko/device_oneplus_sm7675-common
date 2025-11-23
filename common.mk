1521e0d166cc86b1057a483bb0ae18ee9a32399d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    sm8650-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- common.mk
+++ common.mk
@@ -440,7 +440,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.oplus
 
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,USE_OPLUSTOUCH,true)
+$(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,USE_OPLUSTOUCH,true)
 endif
 
 # Virtualization service
